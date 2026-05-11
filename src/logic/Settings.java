b740f7341bf14fab573ebfbec146f60a8806cdd9
123-Game <aefoster74@gmail.com>
2026-05-11 17:56:59 +0100

    Added creation of data directory if missing

--- src/logic/Settings.java
+++ src/logic/Settings.java
@@ -9,29 +9,69 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Settings {
+    private Map<String, String> getDefaultSettings() {
+        Map<String, String> defaultData = new HashMap<>();
+        defaultData.put("veryHighBg", "16.0");
+        defaultData.put("highBg", "10.0");
+        defaultData.put("lowBg", "3.9");
+        defaultData.put("veryLowBg", "2.0");
+        defaultData.put("checkInterval", "1");
+        defaultData.put("mgdl", "false");
+        defaultData.put("nsurl", "");
+        defaultData.put("apiSecret", "");
+        defaultData.put("winx", "900");
+        defaultData.put("winy", "500");
+        defaultData.put("useHttp", "false");
+        defaultData.put("alignRight", "false");
+        defaultData.put("runOnStartup", "false");
+        return defaultData;
+    }
+
     public Map<String, String> read() {
-        File settingsFile = new File("./data/.settings");
+        File settingsFile = new File("data", ".settings");
 
         try (Scanner reader = new Scanner(settingsFile)) {
-            Map<String, String> data = new HashMap<>();
+            Map<String, String> data = getDefaultSettings();
 
             while (reader.hasNextLine()) {
-                String[] setting = reader.nextLine().split("=");
-                data.put(setting[0], setting[1]);
+                String line = reader.nextLine();
+                int index = line.indexOf('=');
+                if (index != -1) {
+                    data.put(line.substring(0, index), line.substring(index + 1));
+                }
             }
 
             return data;
         } catch (FileNotFoundException error) {
-            error.printStackTrace();
-
             fix();
 
-            return new HashMap<>();
+            return loadDefaultOrRead(settingsFile);
+        }
+    }
+
+    private Map<String, String> loadDefaultOrRead(File settingsFile) {
+        Map<String, String> data = getDefaultSettings();
+        try (Scanner reader = new Scanner(settingsFile)) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                int index = line.indexOf('=');
+                if (index != -1) {
+                    data.put(line.substring(0, index), line.substring(index + 1));
+                }
+            }
+            return data;
+        } catch (FileNotFoundException e) {
+            return data;
         }
     }
 
     public boolean write(Map<String, String> settingsList) {
-        File settingsFile = new File("./data/.settings");
+        File settingsFile = new File("data", ".settings");
+
+        File parentDir = settingsFile.getAbsoluteFile().getParentFile();
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+        }
 
         String settingsJoined =
         "veryHighBg=" + settingsList.get("veryHighBg") +
@@ -45,7 +85,8 @@ public class Settings {
         "\nwinx=" + settingsList.get("winx") +
         "\nwiny=" + settingsList.get("winy") +
         "\nuseHttp=" + settingsList.get("useHttp") +
-        "\nalignRight=" + (settingsList.get("alignRight") != null ? settingsList.get("alignRight") : "false");
+        "\nalignRight=" + (settingsList.get("alignRight") != null ? settingsList.get("alignRight") : "false") +
+        "\nrunOnStartup=" + (settingsList.get("runOnStartup") != null ? settingsList.get("runOnStartup") : "false");
 
         try (FileWriter writer = new FileWriter(settingsFile)) {
             writer.write(settingsJoined);
@@ -63,7 +104,12 @@ public class Settings {
     }
 
     private void fix() {
-        File settingsFile = new File("./data/.settings");
+        File settingsFile = new File("data", ".settings");
+
+        File parentDir = settingsFile.getAbsoluteFile().getParentFile();
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+        }
 
         String settingsJoined =
         "veryHighBg=16.0" +
@@ -77,7 +123,8 @@ public class Settings {
         "\nwinx=900" +
         "\nwiny=500" +
         "\nuseHttp=false" +
-        "\nalignRight=false";
+        "\nalignRight=false" +
+        "\nrunOnStartup=false";
 
         try (FileWriter writer = new FileWriter(settingsFile)) {
             writer.write(settingsJoined);
